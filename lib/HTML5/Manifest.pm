a9957e134e7ff0d1d760bb3023c268c8f115b231
Kazuhiro Osawa <yappo@shibuya.pl>
2011-01-28 17:22:29 +0900

    initial commit

---
+++ lib/HTML5/Manifest.pm
@@ -0,0 +1,32 @@
+package HTML5::Manifest;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML5::Manifest -
+
+=head1 SYNOPSIS
+
+  use HTML5::Manifest;
+
+=head1 DESCRIPTION
+
+HTML5::Manifest is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
